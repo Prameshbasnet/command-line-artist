1e6ed1f1e54c6d8b33652ddc81cb39cbe42b4944
Pramesh Basnet <93832790+Prameshbasnet@users.noreply.github.com>
2025-05-11 22:21:01 +0545

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -7,5 +7,5 @@ export const aboutData = {
   website: "www.prameshbasnet.com.np",
   linkedin: "https://www.linkedin.com/in/prameshbasnet1/",
   github: "https://github.com/prameshbasnet1",
-  summary: "Proactive software developer with experience in delivering full-stack solutions across .NET, React, Flutter, and AWS. Specializes in creating scalable, user-focused applications that optimize business workflows and enhance user experience."
+  summary: "Software developer with experience in delivering full-stack solutions across .NET, React, Flutter, and AWS. Specializes in creating scalable, user-focused applications that optimize business workflows and enhance user experience."
 };
